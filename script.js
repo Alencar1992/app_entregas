9daf36ccba48404f321fa2a8d3664ab5d398d186
Alencar1992 <lucas.aluncar.l70@gmail.com>
2025-12-22 15:50:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,17 @@ let mensagemWhatsApp = "";
 
 function calcularFrete() {
   const km = Number(document.getElementById("km").value);
-
   const valorPorKm = 1.90;
   const taxaMinima = 5.00;
-  const limiteKm = 15;
-  const taxaExtraPorKm = 0.50;
 
   if (!km || km <= 0) {
     alert("Informe a quilometragem da entrega");
     return;
   }
 
-  // Valor base (somente ida)
   let valor = km * valorPorKm;
   let aviso = "";
 
-  // Regra para km acima de 15
-  if (km > limiteKm) {
-    const taxaExtra = (km / 2) * taxaExtraPorKm;
-    valor += taxaExtra;
-    aviso = `🚨 Distância acima de 15 km: aplicada taxa adicional de retorno`;
-  }
-
-  // Taxa mínima
   if (valor < taxaMinima) {
     valor = taxaMinima;
     aviso = "⚠️ Aplicada taxa mínima";
@@ -33,12 +21,10 @@ function calcularFrete() {
   const valorFormatado = valor.toFixed(2).replace(".", ",");
 
   mensagemWhatsApp =
-`Oi 😊
+`Olá 😊
 
 O valor do seu frete ficou em *R$ ${valorFormatado}* 🛵💨
 
-📍 Distância: ${km} km
-${aviso ? "ℹ️ " + aviso + "\n" : ""}
 📆Quando será a entrega?
 👤Quem vai receber?
 ⌚Qual horario para ser entregue?
