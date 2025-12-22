a29118685d31138bc596a6bbc1f5a1f7ed3b79d7
Alencar1992 <lucas.aluncar.l70@gmail.com>
2025-12-22 14:05:31 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function calcularFrete() {
+  const km = Number(document.getElementById("km").value);
+
+  const valorPorKm = 1.90;
+  const taxaMinima = 5.00;
+
+  if (km <= 0 || isNaN(km)) {
+    alert("Informe uma quilometragem válida");
+    return;
+  }
+
+  let valor = km * valorPorKm;
+  let mensagem = "";
+
+  if (valor < taxaMinima) {
+    valor = taxaMinima;
+    mensagem = "⚠️ Aplicada taxa mínima";
+  }
+
+  document.getElementById("resultado").innerHTML =
+    `💰 Valor do frete: <strong>R$ ${valor.toFixed(2)}</strong><br>${mensagem}`;
+}
