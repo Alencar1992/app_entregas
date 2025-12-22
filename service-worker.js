bbe786c71be09d8324cd7190dbb6e2f076cb4177
Alencar1992 <lucas.aluncar.l70@gmail.com>
2025-12-22 16:07:34 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,5 @@
-const CACHE_NAME = "frete-app-v1";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
+<script>
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service-worker.js');
+  }
+</script>
